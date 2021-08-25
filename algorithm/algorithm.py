5907b7cc94ff988be85ea5204610e69b03ae015f
Hyojin Park <68233870+prkhyo@users.noreply.github.com>
2021-08-25 16:37:46 +0000

    greedy Q1

---
+++ algorithm/algorithm.py
@@ -0,0 +1,12 @@
+
+
+# 수행 시간 측정 소스코드 예제
+
+import time
+start_time = time.time()   # 측정 시작
+
+# 프로그램 소스코드 작성...
+
+end_time = time.time()     # 측정 종료  
+
+print("time:", end_time - start_time)   # 수행 시간 출력
