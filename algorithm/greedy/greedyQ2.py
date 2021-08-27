f80666d8a1d846902d7fcf94d1bc08518d756d19
Hyojin Park <68233870+prkhyo@users.noreply.github.com>
2021-08-27 18:30:09 +0000

    greedy Q2

--- algorithm/greedy/greedyQ2.py
+++ algorithm/greedy/greedyQ2.py
@@ -33,12 +33,9 @@
 
 
 s = input()
-result = 0
+result = int(s[0])
 
-for i in range(len(s)):
-    if i == 0:
-        result = int(s[i])
-        continue
+for i in range(1, len(s)):
     if result == 0:
        result += int(s[i])
     else:
@@ -50,19 +47,30 @@ print(result)
     
   
 # 3. 문제 해결 아이디어
-
-
-
-
-
-# 4. 정당성 분석 
-
-
+# 대부분의 경우 '+' 보다 'x'가 더 값을 크게 만듬
+# 다만 두 수 중에서 하나라도 '0' 혹은 '1'인 경우, 곱하기보다는 더하기를 수행하는 것이 효율적
+# 따라서 두 수에 대하여 연산을 수행할 때, 두 수 중에서 하나라도 1이하인 경우에는 더하며,
+# 두 수가 모두 2 이상인 경우에는 곱하면 정답 
 
 
 
 
 # 5. 답안 예시  
+
+data = input()
+
+# 첫 번째 문자를 숫자로 변경하여 대입
+result = int(data[0])
+
+for i in range(1, len(data)):
+    # 두 수 중에서 하나라도 '0' 혹은  '1'인 경우, 곱하기보다는 더하기 수행
+    num = int(data[i])
+    if num <= 1 or result <= 1:
+        result += num
+    else:
+        result *= num
+
+print(result)
 
 
 
