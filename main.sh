babe624a70246f3d3701a54f2f2ab335524794a6
Hyojin Park <68233870+prkhyo@users.noreply.github.com>
2021-09-01 19:36:03 +0000

    programmers greedy question

--- main.sh
+++ main.sh
@@ -1,2 +1,3 @@
 # python  algorithm/Baekjoon-algorithm/greedy/9237.py
-  python  algorithm/greedy/greedyQ3.py
+  # python  algorithm/greedy/greedyQ3.py
+    python  algorithm/programmers-algorithm/greedy/체육복.py
