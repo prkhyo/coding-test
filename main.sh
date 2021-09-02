b0e2f7a770c921a4dd0f22b42dcef74682140225
Hyojin Park <68233870+prkhyo@users.noreply.github.com>
2021-09-02 18:52:11 +0000

    greeady

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 # python  algorithm/Baekjoon-algorithm/greedy/9237.py
   # python  algorithm/greedy/greedyQ3.py
-    python  algorithm/programmers-algorithm/greedy/체육복.py
+    python  algorithm/programmers-algorithm/greedy/큰수만들기.py
